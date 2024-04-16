395fed47748d9c8fd71d9232d81ed99371f4ef46
Tobiasz <tobiasz.sikora2003@gmail.com>
2024-04-17 00:46:09 +0200

    Collected site's filters data

---
+++ Crucial_Data_Files/Filters_Variables.py
@@ -0,0 +1,52 @@
+# Aktualne Konkursy
+
+Fundusze_AK = {'&amp;cats%5B630%5D=631': 'Centralny',
+               '&amp;cats%5B630%5D=632': 'Dzielnicowy',
+               '&amp;cats%5B630%5D=633': 'EOG, CH',
+               '&amp;cats%5B630%5D=634': 'Marszałkowski',
+               '&amp;cats%5B630%5D=635': 'Miejsko-gminny',
+               '&amp;cats%5B630%5D=636': 'Powiatowy',
+               '&amp;cats%5B630%5D=637': 'Prywatny',
+               '&amp;cats%5B630%5D=638': 'Strukturalny',
+               '&amp;cats%5B630%5D=639': 'Stypendialny',
+               '&amp;cats%5B630%5D=640': 'Unijny',
+               '&amp;cats%5B630%5D=3590': 'Wojewódzki',
+               '&amp;cats%5B630%5D=642': 'Zagraniczny'}
+Koniec_Naboru_AK = {'&amp;when=ever': 'Kiedykolwiek','&amp;when=week': 'W tym tygodniu','&amp;when=month': 'W tym miesiącu','&amp;when=next_month': 'W następnym miesiącu','&amp;when=year': 'W tym roku','&amp;when=next_year': 'W przyszłym roku'}
+
+Temat_AK = { '&cats%5B464%5D=4286': 'Ukraina', '&cats%5B464%5D=514': 'Zarządzanie, księgowość', '&cats%5B464%5D=513': 'Finansowanie', '&cats%5B464%5D=515': 'Prawo i obowiązki', '&cats%5B464%5D=512': 'Pożytek publiczny i (1%) 1,5%', '&cats%5B464%5D=504': 'Promocja, media społecznościowe', '&cats%5B464%5D=501': 'Technologie', '&cats%5B464%5D=511': 'Wzmacnianie III sektora', '&cats%5B464%5D=2895': 'Rozwój zawodowy i osobisty', '&cats%5B464%5D=516': 'Badania III sektora', '&cats%5B464%5D=490': 'Aktywność obywatelska', '&cats%5B464%5D=491': 'Prawa człowieka', '&cats%5B464%5D=507': 'Działalność lokalna', '&cats%5B464%5D=492': 'Współpraca z administracją', '&cats%5B464%5D=509': 'Wolontariat', '&cats%5B464%5D=493': 'Dobroczynność, filantropia', '&cats%5B464%5D=510': 'Współpraca z biznesem, CSR', '&cats%5B464%5D=498': 'Ekonomia, rynek pracy', '&cats%5B464%5D=494': 'Działalność międzynarodowa', '&cats%5B464%5D=495': 'Unia Europejska', '&cats%5B464%5D=496': 'Edukacja i wychowanie', '&cats%5B464%5D=497': 'Ekologia, zwierzęta', '&cats%5B464%5D=499': 'Kultura i tradycja', '&cats%5B464%5D=506': 'Religia', '&cats%5B464%5D=500': 'Nauka i technika', '&cats%5B464%5D=505': 'Polityka społeczna', '&cats%5B464%5D=502': 'Ochrona zdrowia', '&cats%5B464%5D=503': 'Bezpieczeństwo', '&cats%5B464%5D=508': 'Sport, czas wolny' }
+
+Odbiorcy_AK = { '&cats%5B626%5D=629': 'Chorzy', '&cats%5B626%5D=2197': 'Cudzoziemcy, migranci', '&cats%5B626%5D=2198': 'Dorośli', '&cats%5B626%5D=2199': 'Dzieci', '&cats%5B626%5D=2200': 'Kobiety', '&cats%5B626%5D=2201': 'Konsumenci', '&cats%5B626%5D=2202': 'Mężczyźni', '&cats%5B626%5D=2203': 'Młodzież', '&cats%5B626%5D=2204': 'Mniejszości narodowe', '&cats%5B626%5D=2207': 'Organizacje pozarządowe', '&cats%5B626%5D=628': 'Osoby bezrobotne', '&cats%5B626%5D=2205': 'Osoby LGBT+', '&cats%5B626%5D=4142': 'Osoby uzależnione', '&cats%5B626%5D=627': 'Osoby w kryzysie bezdomności', '&cats%5B626%5D=2206': 'Osoby z niepełnosprawnościami', '&cats%5B626%5D=2208': 'Rodziny', '&cats%5B626%5D=2209': 'Seniorzy', '&cats%5B626%5D=2210': 'Wolontariusze' }
+
+# Dofinansowanie   &percent%5Bfrom%5D=33&percent%5Bto%5D=44
+
+# Wyniki konkursów 
+
+Fundusze_WK = { '&cats%5B630%5D=631': 'Centralny', '&cats%5B630%5D=632': 'Dzielnicowy', '&cats%5B630%5D=633': 'EOG, CH', '&cats%5B630%5D=634': 'Marszałkowski', '&cats%5B630%5D=635': 'Miejsko-gminny', '&cats%5B630%5D=636': 'Powiatowy', '&cats%5B630%5D=637': 'Prywatny', '&cats%5B630%5D=638': 'Strukturalny', '&cats%5B630%5D=639': 'Stypendialny', '&cats%5B630%5D=640': 'Unijny', '&cats%5B630%5D=3590': 'Wojewódzki', '&cats%5B630%5D=642': 'Zagraniczny' }
+
+Koniec_Naboru_WK = { '': 'Kiedykolwiek', '?when=week': 'W tym tygodniu', '?when=month': 'W tym miesiącu', '?when=next_month': 'W następnym miesiącu', '?when=year': 'W tym roku', '?when=next_year': 'W przyszłym roku' }
+
+Temat_WK = { '&cats%5B464%5D=4286': 'Ukraina', '&cats%5B464%5D=514': 'Zarządzanie, księgowość', '&cats%5B464%5D=513': 'Finansowanie', '&cats%5B464%5D=515': 'Prawo i obowiązki', '&cats%5B464%5D=512': 'Pożytek publiczny i (1%) 1,5%', '&cats%5B464%5D=504': 'Promocja, media społecznościowe', '&cats%5B464%5D=501': 'Technologie', '&cats%5B464%5D=511': 'Wzmacnianie III sektora', '&cats%5B464%5D=2895': 'Rozwój zawodowy i osobisty', '&cats%5B464%5D=516': 'Badania III sektora', '&cats%5B464%5D=490': 'Aktywność obywatelska', '&cats%5B464%5D=491': 'Prawa człowieka', '&cats%5B464%5D=507': 'Działalność lokalna', '&cats%5B464%5D=492': 'Współpraca z administracją', '&cats%5B464%5D=509': 'Wolontariat', '&cats%5B464%5D=493': 'Dobroczynność, filantropia', '&cats%5B464%5D=510': 'Współpraca z biznesem, CSR', '&cats%5B464%5D=498': 'Ekonomia, rynek pracy', '&cats%5B464%5D=494': 'Działalność międzynarodowa', '&cats%5B464%5D=495': 'Unia Europejska', '&cats%5B464%5D=496': 'Edukacja i wychowanie', '&cats%5B464%5D=497': 'Ekologia, zwierzęta', '&cats%5B464%5D=499': 'Kultura i tradycja', '&cats%5B464%5D=506': 'Religia', '&cats%5B464%5D=500': 'Nauka i technika', '&cats%5B464%5D=505': 'Polityka społeczna', '&cats%5B464%5D=502': 'Ochrona zdrowia', '&cats%5B464%5D=503': 'Bezpieczeństwo', '&cats%5B464%5D=508': 'Sport, czas wolny' }
+
+# Dodac page 1 = ?page=1
+
+# Archiwum
+
+Fundusze_A = { '&cats%5B630%5D=631': 'Centralny', '&cats%5B630%5D=632': 'Dzielnicowy', '&cats%5B630%5D=633': 'EOG, CH', '&cats%5B630%5D=634': 'Marszałkowski', '&cats%5B630%5D=635': 'Miejsko-gminny', '&cats%5B630%5D=636': 'Powiatowy', '&cats%5B630%5D=637': 'Prywatny', '&cats%5B630%5D=638': 'Strukturalny', '&cats%5B630%5D=639': 'Stypendialny', '&cats%5B630%5D=640': 'Unijny', '&cats%5B630%5D=3590': 'Wojewódzki', '&cats%5B630%5D=642': 'Zagraniczny' }
+
+Temat_A = { '&cats%5B464%5D=4286': 'Ukraina', '&cats%5B464%5D=514': 'Zarządzanie, księgowość', '&cats%5B464%5D=513': 'Finansowanie', '&cats%5B464%5D=515': 'Prawo i obowiązki', '&cats%5B464%5D=512': 'Pożytek publiczny i (1%) 1,5%', '&cats%5B464%5D=504': 'Promocja, media społecznościowe', '&cats%5B464%5D=501': 'Technologie', '&cats%5B464%5D=511': 'Wzmacnianie III sektora', '&cats%5B464%5D=2895': 'Rozwój zawodowy i osobisty', '&cats%5B464%5D=516': 'Badania III sektora', '&cats%5B464%5D=490': 'Aktywność obywatelska', '&cats%5B464%5D=491': 'Prawa człowieka', '&cats%5B464%5D=507': 'Działalność lokalna', '&cats%5B464%5D=492': 'Współpraca z administracją', '&cats%5B464%5D=509': 'Wolontariat', '&cats%5B464%5D=493': 'Dobroczynność, filantropia', '&cats%5B464%5D=510': 'Współpraca z biznesem, CSR', '&cats%5B464%5D=498': 'Ekonomia, rynek pracy', '&cats%5B464%5D=494': 'Działalność międzynarodowa', '&cats%5B464%5D=495': 'Unia Europejska', '&cats%5B464%5D=496': 'Edukacja i wychowanie', '&cats%5B464%5D=497': 'Ekologia, zwierzęta', '&cats%5B464%5D=499': 'Kultura i tradycja', '&cats%5B464%5D=506': 'Religia', '&cats%5B464%5D=500': 'Nauka i technika', '&cats%5B464%5D=505': 'Polityka społeczna', '&cats%5B464%5D=502': 'Ochrona zdrowia', '&cats%5B464%5D=503': 'Bezpieczeństwo', '&cats%5B464%5D=508': 'Sport, czas wolny' }
+
+
+# https://fundusze.ngo.pl {Typ_Konkursu.txt} ?page=1 {Typ_Lokalizacja.txt} {Lokalizacja_Dokladna.txt} 
+
+# Aktualne Konkursy -> {AK_Fundusze.txt} {AK_Temat1.txt} {AK_Temat2.txt} {AK_Odbiorcy1.txt} {AK_Odbiorcy2.txt} 
+# Wyniki konkursów -> {WK_Fundusze.txt} {WK_Koniec_Naboru.txt} {WK_Temat1.txt} {WK_Temat2.txt}
+# Archiwum -> {A_Fundusze.txt} {A_Temat1.txt} {A_Temat2.txt}
+
+
+'User_Link_Files.py/Typ_Konkursu.txt','Lokalizacja_Dokladna.txt'
+
+
+'User_Link_Files.py/AK_Fundusze.txt','User_Link_Files.py/AK_Temat1.txt','User_Link_Files.py/AK_Temat2.txt','User_Link_Files.py/AK_Odbiorcy1.txt','User_Link_Files.py/AK_Odbiorcy2.txt'
+'User_Link_Files.py/WK_Fundusze.txt','User_Link_Files.py/WK_Koniec_Naboru.txt','User_Link_Files.py/WK_Temat1.txt','User_Link_Files.py/WK_Temat2.txt'
+'User_Link_Files.py/A_Fundusze.txt','User_Link_Files.py/A_Temat1.txt','User_Link_Files.py/A_Temat2.txt'
---
+++ Crucial_Data_Files/Województwa_Filter_Dict.py
@@ -0,0 +1,20 @@
+Wojewodztwa_Filter_Dict = {
+    "Wszystkie": "00",
+    "dolnośląskie": "02",
+    "kujawsko-pomorskie": "04",
+    "lubelskie": "06",
+    "lubuskie": "08",
+    "łódzkie": "10",
+    "małopolskie": "12",
+    "mazowieckie": "14",
+    "opolskie": "16",
+    "podkarpackie": "18",
+    "podlaskie": "20",
+    "pomorskie": "22",
+    "śląskie": "24",
+    "świętokrzyskie": "26",
+    "warmińsko-mazurskie": "28",
+    "wielkopolskie": "30",
+    "zachodniopomorskie": "32"
+}
+
